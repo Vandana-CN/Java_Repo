aa0e1ee878752b78aae3896e754556efa3352b8f
Vandana Cendrollu Nagesh <Vandana.Nagesh@ivycomptech.com>
2022-09-13 17:13:50 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int x;
+        System.out.println("Enter an Year -  ");
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+
+        if ((x % 400 == 0) || ((x % 4 == 0) && (x % 100 != 0)))
+            System.out.println("Leap year");
+        else
+            System.out.println("Not a leap year");
+    }
+}
