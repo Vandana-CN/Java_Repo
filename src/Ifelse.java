d1b47e30f14d5fd7fedb9ccf00745b1c01947443
Vandana Cendrollu Nagesh <Vandana.Nagesh@ivycomptech.com>
2022-09-13 18:36:18 +0530

    GreatestOfThree

---
+++ src/Ifelse.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Ifelse {
+    public static void main(String[] args) {
+        int x,y,z,temp,f;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter - ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = sc.nextInt();
+
+        if(x>y)
+        {
+            temp=x;
+        }
+        else {
+            temp = y;
+        }
+        if(temp>z)
+        {
+         f=temp;
+        }
+        else
+        {
+            f=z;
+        }
+        System.out.println("Largest number - " + f);
+    }
+}
