951781e2cbdb8b89d7e6a5857220bb0bb363e5c3
Vandana Cendrollu Nagesh <Vandana.Nagesh@ivycomptech.com>
2022-09-14 08:26:17 +0530

    SwitchCase

---
+++ src/SwichCase.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SwichCase {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter - ");
+        x = sc.nextInt();
+        switch (x) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+    }
+}
