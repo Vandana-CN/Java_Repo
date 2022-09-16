5fa951dd9b998fa4f717a87ee008f7241df93db3
Vandana-CN <vandanacn99@gmail.com>
2022-09-16 20:47:40 +0530

    commit changes

--- src/EvenNumberFrom1to20.java
+++ src/EvenNumberFrom1to20.java
@@ -1,8 +1,19 @@
 public class EvenNumberFrom1to20 {
     public static void main(String[] args) {
+
+        System.out.println("Even numbers between 1 and 20 -");
         for(int i=1;i<20;i++)
         {
             if (i%2==0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Odd numbers between 1 and 20 -");
+        for(int i=1;i<20;i++)
+        {
+            if (i%2!=0)
             {
                 System.out.print(i + " ");
             }
